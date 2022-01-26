a38d46b6a3b71fd8c91830a1776074d66d668de8
Andersonyu7 <andersonyu614@gmail.com>
2022-01-26 16:16:34 +0800

    test

---
+++ test_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/anderson/test_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/anderson/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/anderson/catkin_ws/devel;/opt/ros/melodic' != '' else []
---
+++ test_ws/build/test/CMakeFiles/normal_turte.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/normal_turte.dir/src/normal_turte.cpp.o"
+  "/home/anderson/test_ws/devel/lib/test/normal_turte.pdb"
+  "/home/anderson/test_ws/devel/lib/test/normal_turte"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/normal_turte.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
